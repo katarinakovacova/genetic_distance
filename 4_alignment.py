7ee019a1b4d8ce33477a6ae9621e7cf7628ca8b2
Katarina Kovacova <katarina.sorens@gmail.com>
2021-10-10 15:09:28 +0200

    jukes_cantor

--- 4_alignment.py
+++ 4_alignment.py
@@ -8,11 +8,43 @@ import sqlite3
 
 random.seed(8)
 
-MAX_ORIGIN = 100
+MAX_ORIGIN = 200
 
 
 def alignment(o1, o2):
     return subprocess.check_output(['alignment', o1['coi5p'], o2['coi5p']]).decode('utf-8').replace('.', '-').split('\n')[:2]
+
+
+def is_same_area(o1, o2):
+    a1 = o1['area']
+    a2 = o2['area']
+
+    if isinstance(a1, str) and isinstance(a2, str):
+        return a1 == a2
+    elif isinstance(a1, list) and isinstance(a2, str):
+        return a2 in a1
+    elif isinstance(a1, str) and isinstance(a2, list):
+        return a1 in a2
+    elif isinstance(a1, list) and isinstance(a2, list):
+        return any(a in a2 for a in a1)
+    else:
+        raise Exception('Unknown area type')
+
+
+def common_area(o1, o2):
+    a1 = o1['area']
+    a2 = o2['area']
+
+    if isinstance(a1, str) and isinstance(a2, str):
+        return a1
+    elif isinstance(a1, list) and isinstance(a2, str):
+        return a2
+    elif isinstance(a1, str) and isinstance(a2, list):
+        return a1
+    elif isinstance(a1, list) and isinstance(a2, list):
+        return ','.join(sorted(a1))
+    else:
+        raise Exception('Unknown area type')
 
 
 mimicry = [f[:-5] for f in os.listdir('mimicry') if f.endswith('.json')]
@@ -45,7 +77,7 @@ for i1 in range(len(genus)):
         
     for i2 in range(i1, len(genus)):
         g2 = genus[i2]
-        
+
         if g1 == g2:
             pass
         elif (g1 in mimicry_dict.keys()) or (g2 in mimicry_dict.keys()):
@@ -63,9 +95,9 @@ for i1 in range(len(genus)):
         
         with open('cooked_data/{}.json'.format(g2), 'r') as f:
             data2 = json.load(f)
-        
+
         print('{} {}'.format(g1, g2))
-        
+
         table_id = '{}_{}'.format(g1.lower(), g2.lower())
         
         sql_table = "CREATE TABLE IF NOT EXISTS {} (pair_id TEXT UNIQUE, area TEXT, seq1 TEXT, seq2 TEXT);".format(table_id)
@@ -73,7 +105,7 @@ for i1 in range(len(genus)):
         cur.execute(sql_table)
         
         cur.execute('BEGIN;')
-        
+
         # two small groups
         if (len(data1) < MAX_ORIGIN) and (len(data2) < MAX_ORIGIN):
             for j1 in range(len(data1)):
@@ -84,11 +116,11 @@ for i1 in range(len(genus)):
                     if origin1['id'] == origin2['id']:
                         continue
                     
-                    if origin1['area'] == origin2['area']:
+                    if is_same_area(origin1, origin2):
                         seq1, seq2 = alignment(origin1, origin2)
-                        pair_id = '{}_{}'.format(origin1['id'], origin2['id'])
+                        pair_id = '{}_{}'.format(*sorted([origin1['id'], origin2['id']]))
                         sql = 'INSERT INTO {} VALUES (?, ?, ?, ?) ON CONFLICT DO NOTHING;'.format(table_id)
-                        cur.execute(sql, [pair_id, origin1['area'], seq1, seq2])
+                        cur.execute(sql, [pair_id, common_area(origin1, origin2), seq1, seq2])
                              
                     
         # one small and one big group
@@ -107,11 +139,11 @@ for i1 in range(len(genus)):
                     if origin1['id'] == origin2['id']:
                         continue
                     
-                    if origin1['area'] == origin2['area']:
+                    if is_same_area(origin1, origin2):
                         seq1, seq2 = alignment(origin1, origin2)
-                        pair_id = '{}_{}'.format(origin1['id'], origin2['id'])
+                        pair_id = '{}_{}'.format(*sorted([origin1['id'], origin2['id']]))
                         sql = 'INSERT INTO {} VALUES (?, ?, ?, ?) ON CONFLICT DO NOTHING;'.format(table_id)
-                        cur.execute(sql, [pair_id, origin1['area'], seq1, seq2])
+                        cur.execute(sql, [pair_id, common_area(origin1, origin2), seq1, seq2])
                         break
                         
         # two big groups 
@@ -125,11 +157,11 @@ for i1 in range(len(genus)):
                     if origin1['id'] == origin2['id']:
                         continue
                     
-                    if origin1['area'] == origin2['area']:
+                    if is_same_area(origin1, origin2):
                         seq1, seq2 = alignment(origin1, origin2)
-                        pair_id = '{}_{}'.format(origin1['id'], origin2['id'])
+                        pair_id = '{}_{}'.format(*sorted([origin1['id'], origin2['id']]))
                         sql = 'INSERT INTO {} VALUES (?, ?, ?, ?) ON CONFLICT DO NOTHING;'.format(table_id)
-                        cur.execute(sql, [pair_id, origin1['area'], seq1, seq2])
+                        cur.execute(sql, [pair_id, common_area(origin1, origin2), seq1, seq2])
                         break
         
         cur.execute('COMMIT;')
